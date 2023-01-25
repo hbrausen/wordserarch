0a76d4da36214c6e22b176e0ba946afa56a586c9
Henry Brausen <hbrausen@ualberta.ca>
2023-01-25 13:21:52 -0700

    Working wordsearch 2d array

--- ws.py
+++ ws.py
@@ -39,5 +39,45 @@ print(chosen)
 # Now we have our words for the wordsearch selected
 # The next step is to lay them out in a grid
 # This is the hard part
+# We will set a target wordsearch puzzle dimension N
+# and then attempt to stuff an NxN grid with the words chosen
+# As much as possible we will try to link words together but we are lazy about
+# it.
 
+"""Create a new empty NxN wordsearch grid"""
+new_grid = lambda N: [['' for i in range(N)] for j in range(N)]
 
+def print_grid(g):
+    print('-'*len(g))
+    for r in g:
+        for c in r:
+            print(c,end='')
+        print()
+
+def merge_grids(g1, g2):
+    for r in range(len(g1)):
+        for c in range(len(g1)):
+            if g2[r][c] is not '':
+                g1[r][c] = g2[r][c]
+
+def place_word(g, w, r, c, v):
+    g2 = new_grid(len(g))
+    if v:
+        it = (1,0)
+    else:
+        it = (0,1)
+    for ch in w:
+        if g[r][c] is not '':
+            return False
+        if r >= len(g) or c >= len(g):
+            return False
+        g2[r][c] = ch
+        r += it[0]
+        c += it[1]
+    merge_grids(g, g2)
+
+g = new_grid(10)
+
+place_word(g, chosen[0], 1, 1, True)
+
+print_grid(g)
